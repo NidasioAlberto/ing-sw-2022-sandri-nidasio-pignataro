09fec50f78e24566e8c30b45ca1023faa53b54c5
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2022-03-15 16:44:50 +0100

    [SchoolBoard] Implemented contructor's exception if the parameter is invalid

--- src/main/java/it/polimi/ingsw/model/SchoolBoard.java
+++ src/main/java/it/polimi/ingsw/model/SchoolBoard.java
@@ -6,54 +6,61 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * This class represents the school board. Every player has one of these, it
- * represents the internal state of each single player and his capabilities to
- * "beat" or not other players during professors and islands exchanges.
+ * This class represents the school board. Every player has one of these, it represents the internal
+ * state of each single player and his capabilities to "beat" or not other players during professors
+ * and islands exchanges.
+ * 
+ * TODO Specify the player's tower color and throw exceptions
  */
 public class SchoolBoard
 {
-    public static final int MAX_TOWERS              = 8;
+    public static final int MAX_TOWERS = 8;
 
-    public static final int MAX_STUDENTS_PER_ROOM   = 10;
+    public static final int MAX_STUDENTS_PER_ROOM = 10;
 
     /**
-     * Dependent on the number of players
+     * Dependent on the number of players.
      */
     public final int MAX_STUDENTS_ENTRANCE;
 
     /**
-     * The entrance zone
+     * The entrance zone.
      */
     private List<Student> entrance;
 
     /**
-     * The dining room zone
+     * The dining room zone.
      */
     private Map<SchoolColor, List<Student>> diningRoom;
 
     /**
-     * The professor table zone
+     * The professor table zone.
      */
     private List<Professor> professorTable;
 
     /**
-     * The towers list
+     * The towers list.
      */
     private List<Tower> towers;
 
     /**
-     * Constructor
+     * Constructor.
+     * 
+     * @throws IllegalArgumentException Thrown if maxStudents is neither 7 nor 9.
      */
-    public SchoolBoard(int maxStudents)
+    public SchoolBoard(int maxStudents) throws IllegalArgumentException
     {
-        this.MAX_STUDENTS_ENTRANCE = maxStudents > 0 ? maxStudents : 7;
-        //Instantiate all the things
-        entrance        = new ArrayList<Student>();
-        diningRoom      = new HashMap<SchoolColor, List<Student>>();
-        professorTable  = new ArrayList<Professor>();
-        towers          = new ArrayList<Tower>();
+        if (maxStudents != 7 && maxStudents != 9)
+            throw new IllegalArgumentException("Invalid max students parameter");
 
-        //Populate the hash map
+        this.MAX_STUDENTS_ENTRANCE = maxStudents;
+        // Instantiate all the things
+        entrance = new ArrayList<Student>();
+        diningRoom = new HashMap<SchoolColor, List<Student>>();
+        professorTable = new ArrayList<Professor>();
+        towers = new ArrayList<Tower>();
+
+        // Populate the hash map
         diningRoom.put(SchoolColor.BLUE, new ArrayList<Student>());
         diningRoom.put(SchoolColor.GREEN, new ArrayList<Student>());
         diningRoom.put(SchoolColor.PINK, new ArrayList<Student>());
@@ -62,129 +69,116 @@ public class SchoolBoard
     }
 
     /**
-     * Adds the professor to the professors table
-     * @param professor The professor to be added
+     * Adds the professor to the professors table.
      */
     public void addProfessor(Professor professor)
     {
-        //Check if it is not null and not already present
-        if(professor != null && !professorTable.contains(professor))
+        // Check if it is not null and not already present
+        if (professor != null && !professorTable.contains(professor))
         {
             professorTable.add(professor);
         }
     }
 
     /**
-     * Removes the specified professor
-     * @param professor The professor to be removed
+     * Removes the specified professor from the professors table.
      */
     public void removeProfessor(Professor professor)
     {
-        //Check if it is not null and present
-        if(professor != null && professorTable.contains(professor))
+        // Check if it is not null and present
+        if (professor != null && professorTable.contains(professor))
         {
             professorTable.remove(professor);
         }
     }
 
     /**
-     * Adds the tower to the list (with maximum of 8 towers)
-     * @param tower The tower to be added
+     * Adds the tower to the list (with maximum of 8 towers).
      */
     public void addTower(Tower tower)
     {
-        //Check if the tower is not null, isn't already present
-        //and the list doesn't exceed the limit
-        if(tower != null && !towers.contains(tower) && towers.size() < MAX_TOWERS)
+        // Check if the tower is not null, isn't already present
+        // and the list doesn't exceed the limit
+        if (tower != null && !towers.contains(tower) && towers.size() < MAX_TOWERS)
         {
             towers.add(tower);
         }
     }
 
     /**
-     * Removes the tower from the list
-     * @param tower The tower to be removed
+     * Removes the tower from the list.
      */
     public void removeTower(Tower tower)
     {
-        //Checks if the tower is not null and present
-        if(tower != null && towers.contains(tower))
+        // Checks if the tower is not null and present
+        if (tower != null && towers.contains(tower))
         {
             towers.remove(tower);
         }
     }
 
-    /**
-     * Adds the student in the entrance
-     * @param student The student to be added
-     */
     public void addStudentToEntrance(Student student)
     {
-        //Checks if the student is null or present already in the board
-        if(student == null || entrance.contains(student) || entrance.size() >= MAX_STUDENTS_ENTRANCE)
+        // Checks if the student is null or present already in the board
+        if (student == null || entrance.contains(student)
+                || entrance.size() >= MAX_STUDENTS_ENTRANCE)
         {
             return;
         }
 
-        //Check if the student is not present in the dining room
-        if(!diningRoom.get(student.getColor()).contains(student))
+        // Check if the student is not present in the dining room
+        if (!diningRoom.get(student.getColor()).contains(student))
         {
-            //Add the student to the entrance
+            // Add the student to the entrance
             entrance.add(student);
         }
     }
 
     /**
-     * Adds the student to the dining room
-     * @param student The student to be added
+     * Adds the student to the dining room.
      */
     private void addStudentToDiningRoom(Student student)
     {
-        //Check if it is not already present and not null and if the dining room is not full
-        if(student != null && !diningRoom.get(student.getColor()).contains(student) &&
-           diningRoom.get(student.getColor()).size() < MAX_STUDENTS_PER_ROOM)
+        // Check if it is not already present and not null and if the dining room is not full
+        if (student != null && !diningRoom.get(student.getColor()).contains(student)
+                && diningRoom.get(student.getColor()).size() < MAX_STUDENTS_PER_ROOM)
         {
-            //Add the student to the map
+            // Add the student to the map
             diningRoom.get(student.getColor()).add(student);
         }
     }
 
     /**
-     * Removes the student from the entrance
-     * @param student The student to be removed
+     * Removes the student from the entrance.
      */
     public void removeStudentFromEntrance(Student student)
     {
-        //Checks if not null and present in entrance
-        if(student != null && entrance.contains(student))
+        // Checks if not null and present in entrance
+        if (student != null && entrance.contains(student))
         {
             entrance.remove(student);
         }
     }
 
     /**
-     * Moves the student to the dining room from the entrance
-     * @param student The student to be moved
+     * Moves the student to the dining room from the entrance.
      */
     public void moveStudentToDining(Student student)
     {
-        //Check if the student is not null and present in the entrance
-        if(student != null && entrance.contains(student))
+        // Check if the student is not null and present in the entrance
+        if (student != null && entrance.contains(student))
         {
             removeStudentFromEntrance(student);
             addStudentToDiningRoom(student);
         }
     }
 
-    /**
-     * Getters
-     */
     public Professor[] getProfessors()
     {
-        //Create the result array
+        // Create the result array
         Professor[] result = new Professor[professorTable.size()];
 
-        //Fill the result array
+        // Fill the result array
         professorTable.toArray(result);
 
         return result;
@@ -192,10 +186,10 @@ public class SchoolBoard
 
     public Student[] getStudentsInEntrance()
     {
-        //Create the result array
+        // Create the result array
         Student[] result = new Student[entrance.size()];
 
-        //Fill the result array
+        // Fill the result array
         entrance.toArray(result);
 
         return result;
@@ -203,10 +197,10 @@ public class SchoolBoard
 
     public Tower[] getTowers()
     {
-        //Create the result array
+        // Create the result array
         Tower[] result = new Tower[towers.size()];
 
-        //Fill the result array
+        // Fill the result array
         towers.toArray(result);
 
         return result;
