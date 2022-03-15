092436afdf0a38d8f469b200f390676dfd07da05
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2022-03-15 16:32:06 +0100

    [Student] Added student test and implemented contructor's exception

--- src/main/java/it/polimi/ingsw/model/Student.java
+++ src/main/java/it/polimi/ingsw/model/Student.java
@@ -1,29 +1,30 @@
 package it.polimi.ingsw.model;
 
 /**
- * This class represents the student disc. It can be assigned to islands
- * and boards and represents one of the three main elements of the game with
- * professors and towers
+ * This class represents the student disc. Each student has its own color which is assigned once and
+ * can't be modified. Students can be assigned to islands and boards and are one of the three main
+ * elements of the game along with professors and towers.
  */
 public class Student
 {
-    /**
-     * The disc color (enum)
-     */
     private SchoolColor color;
 
     /**
-     * Constructor
-     * @param color the disc color
+     * Constructor.
+     * 
+     * @param color The student's disc color.
+     * @throws NullPointerException Thrown if the specified color is invalid.
      */
-    public Student(SchoolColor color)
+    public Student(SchoolColor color) throws NullPointerException
     {
-        this.color = color == null ? SchoolColor.BLUE : color;
+        if (color == null)
+            throw new NullPointerException("A null color was provided");
+
+        this.color = color;
     }
 
-    /**
-     * Disc color getter
-     * @return The SchoolColor of the disc
-     */
-    public SchoolColor getColor() { return color; }
+    public SchoolColor getColor()
+    {
+        return color;
+    }
 }
---
+++ src/test/java/it/polimi/ingsw/StudentTest.java
@@ -0,0 +1,31 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.model.Student;
+import it.polimi.ingsw.model.SchoolColor;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentTest
+{
+    /**
+     * For each color, create a student for that color and check it's color.
+     */
+    @Test
+    public void checkStudentColorTest()
+    {
+        for (SchoolColor color : SchoolColor.values())
+        {
+            Student s = new Student(color);
+            assertEquals(s.getColor(), color);
+        }
+    }
+
+    /**
+     * The student constructor should throw NullPointerException if an invalid parameter is passed.
+     */
+    @Test
+    public void nullParameterTest()
+    {
+        assertThrows(NullPointerException.class, () -> new Student(null));
+    }
+}
